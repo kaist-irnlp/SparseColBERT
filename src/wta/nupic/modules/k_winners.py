8db350cd070d5773e301eeea2654565dac9747e6
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-09-17 13:17:17 +0900

    fix WTA for 3D tensor

--- src/wta/nupic/modules/k_winners.py
+++ src/wta/nupic/modules/k_winners.py
@@ -262,7 +262,7 @@ class KWinners(KWinnersBase):
         self.learning_iterations += batch_size
         period = min(self.duty_cycle_period, self.learning_iterations)
         self.duty_cycle.mul_(period - batch_size)
-        self.duty_cycle.add_(x.gt(0).sum(dim=0, dtype=torch.float))
+        self.duty_cycle.add_(x > 0).mean(dim=1, dtype=torch.float)
         self.duty_cycle.div_(period)
 
     def extra_repr(self):
