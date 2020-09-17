3846aae6a46dab78f48997716d224d190e2838e5
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-09-17 13:03:44 +0900

    initial

--- src/model.py
+++ src/model.py
@@ -1,20 +1,25 @@
+from .wta import WTAModel
 import string
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from omegaconf import OmegaConf
 
 from transformers import BertPreTrainedModel, BertModel, BertTokenizer
 from src.parameters import DEVICE
 
 
 class ColBERT(BertPreTrainedModel):
-    def __init__(self, config, query_maxlen, doc_maxlen, dim=128, similarity_metric='cosine'):
+    def __init__(
+        self, config, query_maxlen, doc_maxlen, dim=128, similarity_metric="cosine"
+    ):
         super(ColBERT, self).__init__(config)
 
         self.query_maxlen = query_maxlen
         self.doc_maxlen = doc_maxlen
         self.similarity_metric = similarity_metric
 
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
         self.skiplist = {w: True for w in string.punctuation}
 
         self.bert = BertModel(config)
@@ -28,41 +33,56 @@ class ColBERT(BertPreTrainedModel):
     def query(self, queries):
         queries = [["[unused0]"] + self._tokenize(q) for q in queries]
 
-        input_ids, attention_mask = zip(*[self._encode(x, self.query_maxlen) for x in queries])
-        input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(attention_mask)
+        input_ids, attention_mask = zip(
+            *[self._encode(x, self.query_maxlen) for x in queries]
+        )
+        input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(
+            attention_mask
+        )
 
         Q = self.bert(input_ids, attention_mask=attention_mask)[0]
         Q = self.linear(Q)
 
-        return torch.nn.functional.normalize(Q, p=2, dim=2)
+        return torch.nn.functional.normalize(Q, p=2, dim=-1)
 
     def doc(self, docs, return_mask=False):
-        docs = [["[unused1]"] + self._tokenize(d)[:self.doc_maxlen-3] for d in docs]
+        docs = [["[unused1]"] + self._tokenize(d)[: self.doc_maxlen - 3] for d in docs]
 
-        lengths = [len(d)+2 for d in docs]  # +2 for [CLS], [SEP]
+        lengths = [len(d) + 2 for d in docs]  # +2 for [CLS], [SEP]
         d_max_length = max(lengths)
 
         input_ids, attention_mask = zip(*[self._encode(x, d_max_length) for x in docs])
-        input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(attention_mask)
+        input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(
+            attention_mask
+        )
 
         D = self.bert(input_ids, attention_mask=attention_mask)[0]
         D = self.linear(D)
 
         # [CLS] .. d ... [SEP] [PAD] ... [PAD]
-        mask = [[1] + [x not in self.skiplist for x in d] + [1] + [0] * (d_max_length - length)
-                for d, length in zip(docs, lengths)]
+        mask = [
+            [1]
+            + [x not in self.skiplist for x in d]
+            + [1]
+            + [0] * (d_max_length - length)
+            for d, length in zip(docs, lengths)
+        ]
 
         D = D * torch.tensor(mask, device=DEVICE, dtype=torch.float32).unsqueeze(2)
-        D = torch.nn.functional.normalize(D, p=2, dim=2)
+        D = torch.nn.functional.normalize(D, p=2, dim=-1)
 
         return (D, mask) if return_mask else D
 
     def score(self, Q, D):
-        if self.similarity_metric == 'cosine':
+        if self.similarity_metric == "cosine":
             return (Q @ D.permute(0, 2, 1)).max(2).values.sum(1)
-        
-        assert self.similarity_metric == 'l2'
-        return (-1.0 * ((Q.unsqueeze(2) - D.unsqueeze(1))**2).sum(-1)).max(-1).values.sum(-1)
+
+        assert self.similarity_metric == "l2"
+        return (
+            (-1.0 * ((Q.unsqueeze(2) - D.unsqueeze(1)) ** 2).sum(-1))
+            .max(-1)
+            .values.sum(-1)
+        )
 
     def _tokenize(self, text):
         if type(text) == list:
@@ -71,7 +91,9 @@ class ColBERT(BertPreTrainedModel):
         return self.tokenizer.tokenize(text)
 
     def _encode(self, x, max_length):
-        input_ids = self.tokenizer.encode(x, add_special_tokens=True, max_length=max_length)
+        input_ids = self.tokenizer.encode(
+            x, add_special_tokens=True, max_length=max_length
+        )
 
         padding_length = max_length - len(input_ids)
         attention_mask = [1] * len(input_ids) + [0] * padding_length
@@ -81,3 +103,118 @@ class ColBERT(BertPreTrainedModel):
 
     def _tensorize(self, l):
         return torch.tensor(l, dtype=torch.long, device=DEVICE)
+
+
+class SparseColBERT(ColBERT):
+    def __init__(
+        self,
+        config,
+        query_maxlen,
+        doc_maxlen,
+        n,
+        k,
+        dim=128,
+        similarity_metric="cosine",
+    ):
+        super().__init__(config, query_maxlen, doc_maxlen, dim, similarity_metric)
+        # modification
+        self.n = n
+        self.k = k
+        self.dense_size = self.bert.embeddings.word_embeddings.weight.shape[1]
+        wta_params = OmegaConf.create(
+            {
+                "n": n,
+                "k": k,
+                "model": {
+                    "weight_sparsity": 0.3,
+                    "normalize_weights": True,
+                    "k_inference_factor": 1.5,
+                    "boost_strength": 1.5,
+                    "boost_strength_factor": 0.9,
+                    "dense_size": self.dense_size,
+                },
+            }
+        )
+        self.linear = WTAModel(wta_params)
+
+    def _sparse_maxpool(self, T):
+        T_max = torch.max(T, dim=1).values
+        return T_max
+
+    # TODO: need changes
+    def forward(self, Q, D):
+        Q_sp, D_sp = self.query(Q), self.doc(D)
+        Q_sp, D_sp = self._sparse_maxpool(Q_sp), self._sparse_maxpool(D_sp)
+        return self.score(Q_sp, D_sp)
+
+    # TODO: need changes
+    def score(self, Q, D):
+        if self.similarity_metric == "cosine":
+            scores = (Q * D).sum(dim=-1)
+            return scores
+
+        assert self.similarity_metric == "l2"
+        return F.mse_loss(Q, D, reduction="none")
+
+    # def query(self, queries):
+    #     queries = [["[unused0]"] + self._tokenize(q) for q in queries]
+
+    #     input_ids, attention_mask = zip(
+    #         *[self._encode(x, self.query_maxlen) for x in queries]
+    #     )
+    #     input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(
+    #         attention_mask
+    #     )
+
+    #     Q = self.bert(input_ids, attention_mask=attention_mask)[0]
+    #     Q = self.linear(Q)
+
+    #     return torch.nn.functional.normalize(Q, p=2, dim=2)
+
+    # def doc(self, docs, return_mask=False):
+    #     docs = [["[unused1]"] + self._tokenize(d)[: self.doc_maxlen - 3] for d in docs]
+
+    #     lengths = [len(d) + 2 for d in docs]  # +2 for [CLS], [SEP]
+    #     d_max_length = max(lengths)
+
+    #     input_ids, attention_mask = zip(*[self._encode(x, d_max_length) for x in docs])
+    #     input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(
+    #         attention_mask
+    #     )
+
+    #     D = self.bert(input_ids, attention_mask=attention_mask)[0]
+    #     D = self.linear(D)
+
+    #     # [CLS] .. d ... [SEP] [PAD] ... [PAD]
+    #     mask = [
+    #         [1]
+    #         + [x not in self.skiplist for x in d]
+    #         + [1]
+    #         + [0] * (d_max_length - length)
+    #         for d, length in zip(docs, lengths)
+    #     ]
+
+    #     D = D * torch.tensor(mask, device=DEVICE, dtype=torch.float32).unsqueeze(2)
+    #     D = torch.nn.functional.normalize(D, p=2, dim=2)
+
+    #     return (D, mask) if return_mask else D
+
+    # def _tokenize(self, text):
+    #     if type(text) == list:
+    #         return text
+
+    #     return self.tokenizer.tokenize(text)
+
+    # def _encode(self, x, max_length):
+    #     input_ids = self.tokenizer.encode(
+    #         x, add_special_tokens=True, max_length=max_length
+    #     )
+
+    #     padding_length = max_length - len(input_ids)
+    #     attention_mask = [1] * len(input_ids) + [0] * padding_length
+    #     input_ids = input_ids + [103] * padding_length
+
+    #     return input_ids, attention_mask
+
+    # def _tensorize(self, l):
+    #     return torch.tensor(l, dtype=torch.long, device=DEVICE)
---
+++ src/wta/wta.py
@@ -0,0 +1,79 @@
+from torch import nn
+import torch
+import math
+import torch.nn.functional as F
+import abc
+from .nupic import *
+
+
+class WTAModel(nn.Module):
+    def __init__(self, hparams):
+        super().__init__()
+        self.hparams = hparams
+
+        # init weights
+        self._init_layers()
+
+    def forward(self, x):
+        features = self.layers(x)
+        # if self.hparams.model.use_normalized_sparse:
+        #     features = F.normalize(features, dim=-1)
+        return features
+
+    def on_epoch_end(self):
+        self.apply(update_boost_strength)
+        self.apply(rezero_weights)
+
+    @property
+    def entropy(self):
+        return self.kwinners[-1].entropy()
+
+    @property
+    def max_entropy(self):
+        return self.kwinners[-1].max_entropy()
+
+    @property
+    def boost_strength(self):
+        return self.kwinners[-1].boost_strength
+
+    def _init_layers(self):
+        self.layers = nn.Sequential()
+        self.kwinners = []  # for logging
+
+        n = self.hparams.n
+        k = self.hparams.k
+        weight_sparsity = self.hparams.model.weight_sparsity
+        normalize_weights = self.hparams.model.normalize_weights
+        # dropout = self.hparams.model.dropout
+        k_inference_factor = self.hparams.model.k_inference_factor
+        boost_strength = self.hparams.model.boost_strength
+        boost_strength_factor = self.hparams.model.boost_strength_factor
+        next_input_size = self.hparams.model.dense_size
+        for i in range(len(n)):
+            linear = nn.Linear(next_input_size, n[i])
+            if 0 < weight_sparsity < 1:
+                linear = SparseWeights(linear, sparsity=weight_sparsity)
+                if normalize_weights:
+                    linear.apply(normalize_sparse_weights)
+            self.layers.add_module(f"linear_{i+1}", linear)
+            # self.layers.add_module(f"bn_{i+1}", nn.BatchNorm1d(n[i], affine=False))
+            if self.hparams.model.use_activation:
+                self.layers.add_module(f"selu_{i+1}", nn.SELU())
+            # dropout
+            # self.layers.add_module(f"dropout_{i+1}", nn.Dropout(dropout))
+            # add kwinner layer
+            kwinner = KWinners(
+                n=n[i],
+                percent_on=k[i],
+                k_inference_factor=k_inference_factor,
+                boost_strength=boost_strength,
+                boost_strength_factor=boost_strength_factor,
+                break_ties=True,
+                relu=False,
+                inplace=False,
+            )
+            self.layers.add_module(f"kwinner_{i+1}", kwinner)
+            self.kwinners.append(kwinner)
+            next_input_size = n[i]
+        # save output_size
+        self.output_size = next_input_size
