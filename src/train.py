45737b25a9668be7d41fb5e8dc14f2e900716117
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-09-17 13:09:18 +0900

    trivial

--- src/train.py
+++ src/train.py
@@ -13,21 +13,27 @@ def main():
     random.seed(12345)
     torch.manual_seed(1)
 
-    parser = ArgumentParser(description='Training ColBERT with <query, positive passage, negative passage> triples.')
+    parser = ArgumentParser(
+        description="Training ColBERT with <query, positive passage, negative passage> triples."
+    )
 
-    parser.add_argument('--lr', dest='lr', default=3e-06, type=float)
-    parser.add_argument('--maxsteps', dest='maxsteps', default=400000, type=int)
-    parser.add_argument('--bsize', dest='bsize', default=32, type=int)
-    parser.add_argument('--accum', dest='accumsteps', default=2, type=int)
+    parser.add_argument("--lr", dest="lr", default=3e-06, type=float)
+    parser.add_argument("--maxsteps", dest="maxsteps", default=400000, type=int)
+    parser.add_argument("--bsize", dest="bsize", default=32, type=int)
+    parser.add_argument("--accum", dest="accumsteps", default=2, type=int)
 
-    parser.add_argument('--data_dir', dest='data_dir', default=DEFAULT_DATA_DIR)
-    parser.add_argument('--triples', dest='triples', default='triples.train.small.tsv')
-    parser.add_argument('--output_dir', dest='output_dir', default='outputs.train/')
+    parser.add_argument("--data_dir", dest="data_dir", default=DEFAULT_DATA_DIR)
+    parser.add_argument("--triples", dest="triples", default="triples.train.small.tsv")
+    parser.add_argument("--output_dir", dest="output_dir", default="outputs.train/")
 
-    parser.add_argument('--similarity', dest='similarity', default='cosine', choices=['cosine', 'l2'])
-    parser.add_argument('--dim', dest='dim', default=128, type=int)
-    parser.add_argument('--query_maxlen', dest='query_maxlen', default=32, type=int)
-    parser.add_argument('--doc_maxlen', dest='doc_maxlen', default=180, type=int)
+    parser.add_argument(
+        "--similarity", dest="similarity", default="cosine", choices=["cosine", "l2"]
+    )
+    parser.add_argument("--dim", dest="dim", default=128, type=int)
+    parser.add_argument("--query_maxlen", dest="query_maxlen", default=32, type=int)
+    parser.add_argument("--doc_maxlen", dest="doc_maxlen", default=180, type=int)
+    parser.add_argument("--n", default=4096, type=int)
+    parser.add_argument("--k", default=0.005, type=float)
 
     # TODO: Add resume functionality
     # TODO: Save the configuration to the checkpoint.
@@ -38,8 +44,10 @@ def main():
 
     create_directory(args.output_dir)
 
-    assert args.bsize % args.accumsteps == 0, ((args.bsize, args.accumsteps),
-                                               "The batch size must be divisible by the number of gradient accumulation steps.")
+    assert args.bsize % args.accumsteps == 0, (
+        (args.bsize, args.accumsteps),
+        "The batch size must be divisible by the number of gradient accumulation steps.",
+    )
     assert args.query_maxlen <= 512
     assert args.doc_maxlen <= 512
 
