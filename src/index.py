2dc8136316e33d95dc632376e16467cb7b8dc75a
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2020-09-21 13:36:24 +0900

    trivial

--- src/index.py
+++ src/index.py
@@ -14,24 +14,29 @@ from src.indexing.encoder import encode
 def main():
     random.seed(123456)
 
-    parser = ArgumentParser(description='Exhaustive (non-index-based) evaluation of re-ranking with ColBERT.')
+    parser = ArgumentParser(
+        description="Exhaustive (non-index-based) evaluation of re-ranking with ColBERT."
+    )
 
-    parser.add_argument('--index', dest='index', required=True)
-    parser.add_argument('--checkpoint', dest='checkpoint', required=True)
-    parser.add_argument('--collection', dest='collection', default='collection.tsv')
+    parser.add_argument("--index", dest="index", required=True)
+    parser.add_argument("--checkpoint", dest="checkpoint", required=True)
+    parser.add_argument("--collection", dest="collection", default="collection.tsv")
 
-    parser.add_argument('--data_dir', dest='data_dir', default=DEFAULT_DATA_DIR)
-    parser.add_argument('--output_dir', dest='output_dir', default='outputs.index/')
+    parser.add_argument("--data_dir", dest="data_dir", default=DEFAULT_DATA_DIR)
+    parser.add_argument("--output_dir", dest="output_dir", default="outputs.index/")
 
-    parser.add_argument('--bsize', dest='bsize', default=128, type=int)
-    parser.add_argument('--bytes', dest='bytes', default=2, choices=[2, 4], type=int)
-    parser.add_argument('--subsample', dest='subsample', default=None)  # TODO: Add this
+    parser.add_argument("--bsize", dest="bsize", default=128, type=int)
+    parser.add_argument("--bytes", dest="bytes", default=2, choices=[2, 4], type=int)
+    parser.add_argument("--subsample", dest="subsample", default=None)  # TODO: Add this
 
     # TODO: For the following four arguments, default should be None. If None, they should be loaded from checkpoint.
-    parser.add_argument('--similarity', dest='similarity', default='cosine', choices=['cosine', 'l2'])
-    parser.add_argument('--dim', dest='dim', default=128, type=int)
-    parser.add_argument('--query_maxlen', dest='query_maxlen', default=32, type=int)
-    parser.add_argument('--doc_maxlen', dest='doc_maxlen', default=180, type=int)
+    parser.add_argument(
+        "--similarity", dest="similarity", default="cosine", choices=["cosine", "l2"]
+    )
+    parser.add_argument("--dim", dest="dim", default=128, type=int)
+    parser.add_argument("--query_maxlen", dest="query_maxlen", default=32, type=int)
+    parser.add_argument("--doc_maxlen", dest="doc_maxlen", default=180, type=int)
+    parser.add_argument("--dense", action="store_true")
 
     # TODO: Add resume functionality
 
